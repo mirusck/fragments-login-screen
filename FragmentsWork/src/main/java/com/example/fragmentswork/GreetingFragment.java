90c05614e869c3bb13fcceab30fe5ea19a17ecb0
mirus <motriz@i.ua>
2013-10-21 18:49:28 +0300

    useful note

--- FragmentsWork/src/main/java/com/example/fragmentswork/GreetingFragment.java
+++ FragmentsWork/src/main/java/com/example/fragmentswork/GreetingFragment.java
@@ -14,5 +14,5 @@ public class GreetingFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_greeting, null);
     }
-
+    // other stuff to onActivityCreate !
 }
